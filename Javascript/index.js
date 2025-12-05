61708972b65ad859443dcfd076bfb27598e6e724
Karthik B <karthikbalu5533@gmail.com>
2025-12-05 23:55:02 +0530

    finally added js file the coding was bit tough but finally understand how this this works

---
+++ Javascript/index.js
@@ -0,0 +1,25 @@
+function gradeCalculator() {
+
+    let mark1 = Number(prompt("Enter mark for English:"))
+    let mark2 = Number(prompt("Enter mark for Computer Science:"))
+    let mark3 = Number(prompt("Enter mark for Social Studies:"))
+    let mark4 = Number(prompt("Enter mark for Chemistry:"))
+    let mark5 = Number(prompt("Enter mark for Maths:"))
+
+    let totalMarks = mark1 + mark2 + mark3 + mark4 + mark5
+    let Average = totalMarks / 5
+    let grade
+
+    if (Average >= 90) grade = "A+"
+    else if (Average >= 80) grade = "A"
+    else if (Average >= 70) grade = "B"
+    else if (Average >= 60) grade = "C"
+    else if (Average >= 50) grade = "D"
+    else grade = "F"
+
+    alert(
+        "Total Marks: " + totalMarks +
+        "\nAverage: " + Average +
+        "\nGrade: " + grade
+    )
+}
